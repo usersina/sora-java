16282bdede9425cac231075b848f9e6ea6a4eda3
UserSina <benna.anis2@gmail.com>
2021-05-08 15:11:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bankapp_javafx
+ A full CRUD app with javafx & hibernate, uses Maven as a dependency lanager
---
+++ src/main/java/application/hibernate/entity/Person.java
@@ -0,0 +1,59 @@
+package application.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "address")
+	private String address;
+
+	public Person() {
+	}
+
+	public Person(String firstName, String lastName, String address) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
