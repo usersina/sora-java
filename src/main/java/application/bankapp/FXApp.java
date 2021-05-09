b7639c00d108a30c8d37dea3fe594f9cdf73792e
UserSina <benna.anis2@gmail.com>
2021-05-09 13:21:17 +0100

    Make login page

--- src/main/java/application/bankapp/FXApp.java
+++ src/main/java/application/bankapp/FXApp.java
@@ -2,19 +2,32 @@ package application.bankapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 // Do not start app with this, start App.java instead
 public class FXApp extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("scenes/Index.fxml"));
-			Scene scene = new Scene(root, 600, 400);
-			primaryStage.setResizable(false);
+			Parent root = FXMLLoader.load(getClass().getResource("/views/Index.fxml"));
+			// Move window on drag & drop
+			root.setOnMousePressed(pressEvent -> {
+				root.setOnMouseDragged(dragEvent -> {
+					primaryStage.setX(dragEvent.getScreenX() - pressEvent.getSceneX());
+					primaryStage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
+				});
+			});
+
+			// Set window style & title
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setTitle("My demo app");
+			primaryStage.setResizable(false);
+
+			// Create & show the scene
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
--- src/main/java/application/bankapp/controllers/IndexController.java
+++ src/main/java/application/bankapp/controllers/IndexController.java
@@ -1,5 +1,25 @@
 package application.bankapp.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class IndexController {
+
+	@FXML
+	private TextField tfEmail;
+
+	@FXML
+	private PasswordField tfPassword;
+
+	@FXML
+	private Button btnSignIn;
+
+	@FXML
+	void handleExit(MouseEvent event) {
+		System.exit(0);
+	}
 
 }
