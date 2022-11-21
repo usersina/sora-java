d72b673855d3a6b748681ffe6f921d6a6814f817
usersina <benna.anis2@gmail.com>
2022-11-21 11:06:55 +0100

    Replace int to Integer for an actual null

--- src/main/java/application/hibernate/entities/composite/UserCollection.java
+++ src/main/java/application/hibernate/entities/composite/UserCollection.java
@@ -9,18 +9,24 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 
+import org.hibernate.annotations.Type;
+
 import application.hibernate.entities.Artwork;
+import application.hibernate.entities.Audio;
+import application.hibernate.entities.Book;
 import application.hibernate.entities.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity(name = "user_collection")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class UserCollection {
     @EmbeddedId
     private UserCollectionKey id;
@@ -31,16 +37,20 @@ public class UserCollection {
     private Date lastSeenAt;
 
     /**
-     * Relevant only if artwork is an Audio, in milliseconds
+     * Relevant only if artwork is an Audio, in milliseconds. This is set to
+     * Integer so that it can be NULL in the database.
      */
     @Column(nullable = true)
-    private int durationCheckpoint;
+    @Type(type = "int")
+    private Integer durationCheckpoint;
 
     /**
-     * Relevant only if artwork is a Book
+     * Relevant only if artwork is a Book. This is set to Integer so that it
+     * can be NULL in the database.
      */
     @Column(nullable = true)
-    private int pageCheckpoint;
+    @Type(type = "int")
+    private Integer pageCheckpoint;
 
     // --------- Relations --------- //
     @ManyToOne
@@ -52,4 +62,17 @@ public class UserCollection {
     @MapsId("artworkId")
     @JoinColumn(name = "artwork_id")
     Artwork artwork;
+
+    // --------- Constructors --------- //
+    public UserCollection(Date boughtAt, User user, Artwork artwork) {
+        this.id = new UserCollectionKey(user.getId(), artwork.getId());
+        this.boughtAt = boughtAt;
+        this.user = user;
+        this.artwork = artwork;
+        if (this.artwork instanceof Book) {
+            this.pageCheckpoint = 0;
+        } else if (this.artwork instanceof Audio) {
+            this.durationCheckpoint = 0;
+        }
+    }
 }
